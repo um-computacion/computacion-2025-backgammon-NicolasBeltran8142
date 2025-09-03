843b32ef4ed72eb753b864ae3f0ba679795c7144
marquitos8142 <nicolasbeltran880@gmail.com>
2025-09-03 08:57:38 -0300

    Jugadas dobles al sacar dobles con los dados

--- core/dados.py
+++ core/dados.py
@@ -1,10 +1,10 @@
-# core/dados.py
+import random
+
 class Dice:
     def __init__(self):
         self.__values__ = (0, 0)
 
     def roll_dice(self):
-        import random
         die1 = random.randint(1, 6)
         die2 = random.randint(1, 6)
         self.__values__ = (die1, die2)
@@ -18,3 +18,8 @@ class Dice:
 
     def set_values_for_test(self, val1, val2):
         self.__values__ = (val1, val2)
+
+def obtener_movimientos(dado1, dado2):
+    if dado1 == dado2:
+        return [dado1] * 4
+    return [dado1, dado2]
